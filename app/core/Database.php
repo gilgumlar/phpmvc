8ac98bd0286b28fa8e133c0dd9b28f25d32f54e5
dhanakbar <maulana261100@gmail.com>
2021-12-08 12:15:08 +0800

    configurasi database

---
+++ app/core/Database.php
@@ -0,0 +1,36 @@
+<?php
+class Database
+{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $db_name = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name;
+        $option = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRORMODE => PDO::ERRMODE_EXCEPTION
+        ];
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function query($query)
+    {
+        $this->stmt = $his->dbh->prepare($query);
+    }
+
+    // public function bind($param, $value, $type = null)
+    // {
+    //     if (is_null($type)) {
+    //     }
+    // }
+}
--- app/init.php
+++ app/init.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'core/App.php'; //memanggil app pada folder core
 require_once 'core/Controller.php'; //memanggil controller pada folder core
-require_once 'cofig/config.php'; //memanggil config pada folder core
+require_once 'config/config.php'; //memanggil config pada folder core
 require_once 'core/Database.php';
 
 require_once  'core/Flasher.php'; //memanggil flasher pada folder core
